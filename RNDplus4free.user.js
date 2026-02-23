eb1e457d3d38d8af33bf98cd5b689686505df250
mx4k <mx4k@users.noreply.github.com>
2026-02-23 16:21:13 +0100

    Bump version to 0.6.1

--- RNDplus4free.user.js
+++ RNDplus4free.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     RNDplus4free
 // @description Laden des Artikel-Textes aus dem JSON im Quelltext
-// @version  0.6.0
+// @version  0.6.1
 // @match https://*.haz.de/*.html*
 // @match https://*.neuepresse.de/*.html*
 // @match https://*.sn-online.de/*.html*
